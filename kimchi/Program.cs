40a3ca162cf27ef53d7d7ce22a1dbf3e4a9f8b28
ChocoChocobo <sharonovjr@yahoo.com>
2023-02-19 14:19:07 +0300

    Add project files.

---
+++ kimchi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace kimchi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*double x = 3.3;
+            double y = 7.7;
+
+            int z = (int)x;
+            WriteLine(z);*/
+
+            /*WriteLine("Enter your name: ");
+            string name;
+            name = ReadLine();
+            if(name == "") 
+            {
+                WriteLine("Hello world!");
+            }
+            else
+            {
+                WriteLine("Hello, " + name);
+            }*/
+
+            Write("Введите целое число: ");
+            string numberString = ReadLine();
+            int number = Convert.ToInt32(numberString);
+            int number1 = Int32.Parse(numberString);
+            WriteLine("Число: " + number);
+            WriteLine("Число: " + number1);
+        }
+    }
+}
---
+++ kimchi/Ramen.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace kimchi
+{
+    internal class Program
+    {
+        static void startQuiz()
+        {
+            string[] questions =
+            {
+                "Самая высокая гора? ",
+                "Самая длинная река? ",
+                "Самая большая страна? "
+            };
+            string[] answers =
+            {
+                "Эверест",
+                "Амазонка",
+                "Китай"
+            };
+            int counterOfRightAnswers = 0;
+            string userAnswers;
+
+            for(int i = 0; i < questions.Length; i++) 
+            {
+                Write(questions[i]);
+                userAnswers = ReadLine();
+                if (userAnswers == answers[i])
+                {
+                    counterOfRightAnswers++;
+                    WriteLine("Ответ верный!");
+                }
+                else
+                {
+                    WriteLine("Тупица...");
+                }
+            }
+
+            WriteLine("Правильных ответов: " + counterOfRightAnswers);
+        }
+
+        static void guessNumber()
+        {
+            WriteLine("Добро пожаловать в игру \" Угадай число от 0 до 100 \"! ");
+            Random rand = new  Random();
+            int magicNumber = rand.Next(0, 100);
+            int userNumber = 0;
+            int counter = 0;
+            do
+            {
+                WriteLine("Введи число: ");
+                userNumber = Int32.Parse(ReadLine());
+                counter++;
+
+                if (userNumber < magicNumber)
+                {
+                    WriteLine("Введенное число меньше загаданного!");
+                }
+                else if (userNumber > magicNumber)
+                {
+                    WriteLine("Введенное число больше загаданного!");
+                }
+                else if (userNumber == magicNumber)
+                {
+                    WriteLine("А ты хорош, верно отгадал! Загаданное число: " + magicNumber);
+                    WriteLine($"Тебе понадобилось {counter} попыток"); //Интерполированная строка
+                    break;
+                }
+
+            }while(userNumber != magicNumber);
+        }
+
+        static void Main(string[] args)
+        {
+            //startQuiz();
+            guessNumber();
+        }
+    }
+}
